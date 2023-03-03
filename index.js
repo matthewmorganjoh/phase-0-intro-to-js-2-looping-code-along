3a2191e73eee25b5445d3933e3dbba5734977c33
Matthew Morgan <matthewmorganjoh@gmail.com>
2023-03-02 18:08:32 -0700

    complete looping code along

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    //debugger;
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    //debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+
+function writeCards(names, event){
+    const messages = [];
+
+    for (let i = 0; i < names.length; i++){
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+
+const result = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+console.log(result);
+
+
+function countDown(number){
+    while (number >=0){
+        console.log(number);
+        number--;
+    }
+}
